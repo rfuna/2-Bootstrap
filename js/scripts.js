8c34eaa27eac7fdb4a5edd388cde12befb43660f
Ray <rayfunahashi@gmail.com>
2020-08-15 11:47:22 -0400

    week 4 workshop

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,33 @@ $(function () {
       $("#carouselButton").children("i").addClass("fa-pause");
     }
   });
+
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
+    console.log("reserve");
+  });
+
+  $("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+    console.log("reserve");
+  });
+
 });
+
+// Another alternative method with on('click')
+
+// $(function () {
+//   $(".carousel").carousel({ interval: 2000 });
+//   $("#carouselButton").on('click', function () {
+//     if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+//       $(".carousel").carousel("pause");
+//       $("#carouselButton").children("i").removeClass("fa-pause");
+//       $("#carouselButton").children("i").addClass("fa-play");
+//     } else {
+//       $(".carousel").carousel("cycle");
+//       $("#carouselButton").children("i").removeClass("fa-play");
+//       $("#carouselButton").children("i").addClass("fa-pause");
+//     }
+//   });
+
+// });
